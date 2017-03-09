76d4662d18329a40fd49b35bb202d4467d17a30d
Weronika Grela <Weronika Grela>
2017-03-09 17:03:19 +0100

    first commit

---
+++ src/com/sages/hello/Animal.java
@@ -0,0 +1,8 @@
+package com.sages.hello;
+
+public class Animal {
+	String name;
+	public void wydajDzwiek(){
+		System.out.println("MUUUUUUUUUUU!");
+	}
+}
---
+++ src/com/sages/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.sages.hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Hello World\nnowalinia");
+		/*
+		int zmienna = 1;
+		String sss = new String();
+		int[] table = {1,2,3,4};
+		for (int i : table) {
+			System.out.println(i);
+		}
+		Animal animal = new Animal();
+		animal.wydajDzwiek();
+		*/
+	}
+}
